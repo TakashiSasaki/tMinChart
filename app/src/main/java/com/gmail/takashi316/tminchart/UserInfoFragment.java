f15c9df02d36e1b9d0613072fd62a3feed06edf4
Takashi SASAKI <takashi316@gmail.com>
2014-08-04 15:42:50 +0900

    dialog box

--- app/src/main/java/com/gmail/takashi316/tminchart/UserInfoFragment.java
+++ app/src/main/java/com/gmail/takashi316/tminchart/UserInfoFragment.java
@@ -1,6 +1,8 @@
 package com.gmail.takashi316.tminchart;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -65,7 +67,25 @@ public class UserInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_info, container, false);
+        view.findViewById(R.id.buttonUserList).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final CharSequence[] users = {"山田花子", "鈴木一郎", "吉田二郎"};
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("名前を選択してください");
+                        builder.setItems(users,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                });
+                        builder.show();
+                    }//onClick
+                }//OnClickListener
+        );
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
