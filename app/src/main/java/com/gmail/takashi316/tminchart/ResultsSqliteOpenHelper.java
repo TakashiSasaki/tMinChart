2d5f022a759193d32af48c59b9226d306882ab67
Takashi SASAKI <takashi316@gmail.com>
2014-08-15 15:29:08 +0900

    added new columns

--- app/src/main/java/com/gmail/takashi316/tminchart/ResultsSqliteOpenHelper.java
+++ app/src/main/java/com/gmail/takashi316/tminchart/ResultsSqliteOpenHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by sasaki on 2014/08/04.
  */
 public class ResultsSqliteOpenHelper extends SQLiteOpenHelper{
-    static final int DATABASE_VERSION = 1;
+    static final int DATABASE_VERSION = 3;
     static final String DATABASE_FILE_NAME = "Results.sqlite";
 
     public ResultsSqliteOpenHelper(Context context) {
@@ -19,11 +19,21 @@ public class ResultsSqliteOpenHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE ResultsTable (_id INTEGER PRIMARY KEY autoincrement, " +
                 " date TEXT NOT NULL, name TEXT NOT NULL, age INTEGER, sex TEXT, affiliation TEXT, " +
-                " correction TEXT, fatigue TEXT, light_sensor_value REAL, accelerometer TEXT, device TEXT, dpi TEXT, tcon_chart_results TEXT, tmin_chart_results TEXT )");
+                " correction TEXT, fatigue TEXT, faitgueEx TEXT, care TEXT, careEx TEXT, " +
+                " address TEXT," +
+                "light_sensor_value REAL, accelerometer TEXT, device TEXT, dpi TEXT, tcon_chart_results TEXT, tmin_chart_results TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE ResultsTable");
-    }
+        if(oldVersion < 2){
+            db.execSQL("ALTER TABLE ResultsTable ADD COLUMN fatigueEx;");
+            db.execSQL("ALTER TABLE ResultsTable ADD COLUMN care;");
+            db.execSQL("ALTER TABLE ResultsTable ADD COLUMN careEx;");
+        }//if
+
+        if(oldVersion < 3) {
+            db.execSQL("ALTER TABLE ResultsTable ADD COLUMN address;");
+        }
+    }//onUpgrade
 }
