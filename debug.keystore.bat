9846aef61ae28249bd15fa24f9b2b33243ba8184
Takashi SASAKI <takashi316@gmail.com>
2014-08-17 13:04:03 +0900

    bug fix

--- debug.keystore.bat
+++ debug.keystore.bat
@@ -1,9 +1,9 @@
-CHCP
-echo %USERPROFILE%
-dir %USERPROFILE%\.android
+@ECHO OFF
+ECHO %USERPROFILE%
+DIR %USERPROFILE%\.android
 FC debug.keystore %USERPROFILE%\.android\debug.keystore
 IF %ERRORLEVEL% == 0 GOTO END
 COPY  %USERPROFILE%\.android\debug.keystore %USERPROFILE%\.android\debug.keystore.bak
-COPY ./debug.keystore %USERPROFILE%\.android\debug.keystore
+COPY .\debug.keystore %USERPROFILE%\.android\debug.keystore
 :END
 PAUSE
