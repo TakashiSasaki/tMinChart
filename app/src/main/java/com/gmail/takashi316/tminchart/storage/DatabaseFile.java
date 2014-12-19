8a67a261755787c9c19cf17a90f110f3a75ccc0b
Takashi SASAKI <takashi316@gmail.com>
2014-12-19 09:57:57 +0900

    implementing DatabaseFile class

--- app/src/main/java/com/gmail/takashi316/tminchart/storage/DatabaseFile.java
+++ app/src/main/java/com/gmail/takashi316/tminchart/storage/DatabaseFile.java
@@ -1,6 +1,7 @@
 package com.gmail.takashi316.tminchart.storage;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,31 +18,58 @@ import java.util.Date;
  */
 public class DatabaseFile {
     Context context;
-    private String packageName;
-    private File internal;
-    private File privateExternal;
-    private File publicExternal;
-    private File sd;
+    private File databaseFile;
+    private File databaseFileOnExternalStorage;
+    private File databaseFileOnSdCard;
 
-    private StorageRoots storageRoots = new StorageRoots();
-    private PublicExternalDirectories publicExternalDirectories = new PublicExternalDirectories();
-    private PrivateExternalDirectories privateExternalDirectories;
+    final private StorageRoots storageRoots = new StorageRoots();
 
-    public DatabaseFile(Context context, String database_file_name) {
+    public DatabaseFile(Context context, String database_file_name) throws FileNotFoundException {
         final Date current_time = new Date();
         this.context = context;
-        this.packageName = context.getPackageName();
-        this.privateExternalDirectories = new PrivateExternalDirectories(context);
-        this.internal = context.getDatabasePath(database_file_name);
+        final String packageName = context.getPackageName();
+        final PublicExternalDirectories publicExternalDirectories = new PublicExternalDirectories();
+        this.databaseFile = context.getDatabasePath(database_file_name);
+        Log.d(this.getClass().getSimpleName(), "databaseFile = " + this.databaseFile.getAbsolutePath());
+        if(!this.databaseFile.exists()){
+            throw new FileNotFoundException(this.databaseFile.getAbsolutePath());
+        }
 
-        this.privateExternal = new File(privateExternalDirectories.documents, database_file_name + "."+ current_time.getTime());
-        this.publicExternal = new File(publicExternalDirectories.documents, database_file_name + "."+current_time.getTime());
+        if(publicExternalDirectories.documents == null){
+            if(publicExternalDirectories.downloads == null){
+                this.databaseFileOnExternalStorage = null;
+                Log.d(this.getClass().getSimpleName(), "no external directory");
+            } else {
+                this.databaseFileOnExternalStorage = new File(publicExternalDirectories.downloads, database_file_name + "."+ current_time.getTime());
+                Log.d(this.getClass().getSimpleName(), "databaseFileOnExternalStorage = " + this.databaseFileOnExternalStorage);
+            }
+        } else {
+            this.databaseFileOnExternalStorage = new File(publicExternalDirectories.documents, database_file_name + "."+ current_time.getTime());
+            Log.d(this.getClass().getSimpleName(), "databaseFileOnExternalStorage = " + this.databaseFileOnExternalStorage);
+        }//if
 
         final File sd_package_dir = new File(storageRoots.sdRoot, packageName);
         if(!sd_package_dir.isDirectory()) {
+            Log.e(this.getClass().getSimpleName(), "Mkdir " + sd_package_dir);
             sd_package_dir.mkdirs();
         }//if
-        this.sd = new File(sd_package_dir, database_file_name + "."+ current_time.getTime());
+        if(!sd_package_dir.exists()){
+            Log.e(this.getClass().getSimpleName(), "Can't find " + sd_package_dir);
+        }
+        if(!sd_package_dir.isDirectory()) {
+            this.databaseFileOnSdCard = null;
+            Log.d(this.getClass().getSimpleName(), "databaseFileOnSdCard = null");
+        } else {
+            this.databaseFileOnSdCard = new File(sd_package_dir, database_file_name + "." + current_time.getTime());
+            Log.d(this.getClass().getSimpleName(), "databaseFileOnSdCard = " + this.databaseFileOnSdCard);
+        }
+
+        try {
+            PrivateExternalDirectories privateExternalDirectories = new PrivateExternalDirectories(context);
+        } catch (Exception e){
+            Log.d(getClass().getSimpleName(), e.getMessage());
+        }
+
     }//DatabaseFile
 
     static private byte[] getMd(File file) throws FileNotFoundException{
@@ -61,25 +89,25 @@ public class DatabaseFile {
         }//try
     }//getMd5
 
-    private byte[] getInternalMd() throws FileNotFoundException{
-        return getMd(internal);
+    private byte[] getMd() throws FileNotFoundException{
+        return getMd(this.databaseFile);
     }
 
-    private byte[] getPrivateExternalMd() throws FileNotFoundException{
-        return getMd(privateExternal);
+    private byte[] getMdOnExternalStorage() throws FileNotFoundException{
+        return getMd(this.databaseFileOnExternalStorage);
     }
 
-    private byte[] getPublicExternalMd() throws FileNotFoundException {
-        return getMd(publicExternal);
-    }
-
-    private byte[] getSdMd() throws FileNotFoundException{
-        return getMd(this.sd);
+    private byte[] getMdOnSdCard() throws FileNotFoundException {
+        return getMd(this.databaseFileOnSdCard);
     }
 
     private void copyTo(File dst_file) throws FileNotFoundException, IOException{
-        FileChannel src = new FileInputStream(internal).getChannel();
+        Log.d(this.getClass().getSimpleName(), "copy from " + this.databaseFile);
+        FileChannel src = new FileInputStream(this.databaseFile).getChannel();
+        Log.d(this.getClass().getSimpleName(), "trying to copy to " + dst_file);
+        Log.d(getClass().getSimpleName(), "parent directory of destination exists? " + dst_file.getParentFile().isDirectory());
         dst_file.createNewFile();
+        Log.d(this.getClass().getSimpleName(), "created " + dst_file);
         FileChannel dst = new FileOutputStream(dst_file).getChannel();
         try {
             src.transferTo(0, src.size(), dst);
@@ -88,21 +116,17 @@ public class DatabaseFile {
             dst.close();
         }
 
-        byte[] src_md = getMd(internal);
+        byte[] src_md = getMd(this.databaseFile);
         byte[] dst_md = getMd(dst_file);
         if(!src_md.equals(dst_md)) throw new IOException("Message Digest is not identical.");
     }
 
-    public void copyToPrivateExternal() throws FileNotFoundException, IOException{
-        copyTo(privateExternal);
+    public void copyToExternalStorage() throws FileNotFoundException, IOException{
+        copyTo(this.databaseFileOnExternalStorage);
     }
 
-    public void copyToPublicExternal() throws FileNotFoundException, IOException{
-        copyTo(publicExternal);
-    }
-
-    public void copyToSd() throws FileNotFoundException, IOException{
-        copyTo(sd);
+    public void copyToSdCard() throws FileNotFoundException, IOException{
+        copyTo(this.databaseFileOnSdCard);
     }
 
 
