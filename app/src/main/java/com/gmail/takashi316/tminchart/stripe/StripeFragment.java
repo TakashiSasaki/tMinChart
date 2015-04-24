1d6bed1f4714b68c8fd426e25db240671455686f
Takashi SASAKI <takashi316@gmail.com>
2015-04-24 13:41:17 +0900

    renamed to StripeFragment

--- app/src/main/java/com/gmail/takashi316/tminchart/stripe/StripeFragment.java
+++ app/src/main/java/com/gmail/takashi316/tminchart/stripe/StripeFragment.java
@@ -1,24 +1,33 @@
 package com.gmail.takashi316.tminchart.stripe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.gmail.takashi316.tminchart.R;
+import com.gmail.takashi316.tminchart.face.FaceRectangleFragment;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link StripeViewFragment.OnFragmentInteractionListener} interface
+ * {@link StripeFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link StripeViewFragment#newInstance} factory method to
+ * Use the {@link StripeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StripeViewFragment extends Fragment {
+public class StripeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,17 +39,21 @@ public class StripeViewFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    private Button buttonFinishStripe;
+    private TableLayout tableLayoutStripe;
+    private OnFragmentInteractionListener onFragmentInteractionListener;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment StripeViewFragment.
+     * @return A new instance of fragment StripeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StripeViewFragment newInstance(String param1, String param2) {
-        StripeViewFragment fragment = new StripeViewFragment();
+    public static StripeFragment newInstance(String param1, String param2) {
+        StripeFragment fragment = new StripeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -48,7 +61,7 @@ public class StripeViewFragment extends Fragment {
         return fragment;
     }
 
-    public StripeViewFragment() {
+    public StripeFragment() {
         // Required empty public constructor
     }
 
@@ -59,14 +72,62 @@ public class StripeViewFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+        this.onFragmentInteractionListener = (OnFragmentInteractionListener) getActivity();
+    }//onCreate
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stripe_view, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_stripe, container, false);
+        this.buttonFinishStripe = (Button) view.findViewById(R.id.buttonFinishStripe);
+        this.tableLayoutStripe = (TableLayout) view.findViewById(R.id.tableLayoutStripe);
+
+        this.buttonFinishStripe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onFragmentInteractionListener.finishStripe();
+            }
+        });
+
+        final int TEXT_SIZE = 50;
+        final int COLUMNS = 10;
+        final int ROWS = 10;
+        final Context context = this.getActivity();
+
+        TextView text_view_left_top = new TextView(context);
+        text_view_left_top.setTextSize(TEXT_SIZE);
+        text_view_left_top.setText(" ");
+
+        TableRow table_row_column_index = new TableRow(context);
+        table_row_column_index.addView(text_view_left_top);
+
+        for (int i = 1; i <= COLUMNS; ++i) {
+            TextView text_view_column_index = new TextView(context);
+            text_view_column_index.setTextSize(TEXT_SIZE);
+            text_view_column_index.setText(new String(new byte[]{(byte) (i + 64)}));
+            text_view_column_index.setGravity(Gravity.CENTER);
+            table_row_column_index.addView(text_view_column_index);
+        }//for
+
+        tableLayoutStripe.addView(table_row_column_index);
+
+        for (int r = 0; r < ROWS; ++r) {
+            TableRow table_row = new TableRow(context);
+            TextView text_view_row_index = new TextView(context);
+            text_view_row_index.setTextSize(TEXT_SIZE);
+            text_view_row_index.setText(Integer.toString(r));
+            text_view_row_index.setGravity(Gravity.CENTER);
+            table_row.addView(text_view_row_index);
+            ArrayList<StripeView> stripe_views = new ArrayList<StripeView>();
+            for (int column = 1; column < COLUMNS; ++column) {
+                StripeView stripe_view = new StripeView(context, 0.1f, 1.0f, stripe_views);
+                stripe_views.add(stripe_view);
+            }
+            this.tableLayoutStripe.addView(table_row);
+        }//for
+        return view;
+    }//onCreateView
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -105,6 +166,8 @@ public class StripeViewFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+
+        public void finishStripe();
     }
 
 }
