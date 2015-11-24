05d915d6dc4cb1baab27ab0dfdf45944135396a9
Takashi SASAKI <takashi316@gmail.com>
2015-11-25 02:26:06 +0900

    write to database

--- app/src/main/java/com/gmail/takashi316/tminchart/log/LogActivity.java
+++ app/src/main/java/com/gmail/takashi316/tminchart/log/LogActivity.java
@@ -1,6 +1,8 @@
 package com.gmail.takashi316.tminchart.log;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,10 @@ import com.gmail.takashi316.tminchart.R;
 
 public class LogActivity extends Activity {
     Button buttonCreateDatabase;
+    Button buttonWriteDatabase;
+    LogSqliteOpenHelper logSqliteOpenHelper;
+    SQLiteDatabase readableDatabase;
+    SQLiteDatabase writableDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,20 @@ public class LogActivity extends Activity {
         this.buttonCreateDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                LogActivity.this.logSqliteOpenHelper = new LogSqliteOpenHelper(LogActivity.this.getApplicationContext());
+                LogActivity.this.readableDatabase = LogActivity.this.logSqliteOpenHelper.getReadableDatabase();
+                LogActivity.this.writableDatabase = LogActivity.this.logSqliteOpenHelper.getWritableDatabase();
+            }
+        });
+
+        this.buttonCreateDatabase = (Button) this.findViewById(R.id.buttonWriteDatabase);
+        this.buttonWriteDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues content_values = new ContentValues();
+                content_values.put("json", "{'test':'{}'");
+                content_values.put("nanoTime", System.nanoTime());
+                LogActivity.this.writableDatabase.insert("Log", null, content_values);
             }
         });
     }
