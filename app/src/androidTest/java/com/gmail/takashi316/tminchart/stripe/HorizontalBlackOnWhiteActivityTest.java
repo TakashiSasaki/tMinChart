ac659dfdbcae4646bddc69e27854bcfa6a253b1a
Takashi SASAKI <takashi316@gmail.com>
2016-01-28 08:14:39 +0900

    HorizontalBlackOnWhiteActivityTest passed

--- app/src/androidTest/java/com/gmail/takashi316/tminchart/stripe/HorizontalBlackOnWhiteActivityTest.java
+++ app/src/androidTest/java/com/gmail/takashi316/tminchart/stripe/HorizontalBlackOnWhiteActivityTest.java
@@ -1,8 +1,14 @@
 package com.gmail.takashi316.tminchart.stripe;
 
+import android.os.StrictMode;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+
+import com.gmail.takashi316.tminchart.R;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -17,9 +23,23 @@ public class HorizontalBlackOnWhiteActivityTest {
 
     @Before
     public void init() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
     }
 
     @Test
-    public void createActivity() {
+    public void createActivity() throws InterruptedException {
+        Espresso.onView(ViewMatchers.withId(R.id.buttonStart)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeUp());
+        Thread.sleep(1000);
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeUp());
+        Thread.sleep(1000);
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeUp());
+        Thread.sleep(1000);
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeLeft());
+        Thread.sleep(1000);
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeLeft());
+        Thread.sleep(1000);
+        Espresso.onView(ViewMatchers.withId(R.id.fragmentStripe)).perform(ViewActions.swipeRight());
+        Thread.sleep(1000);
     }
 }
