38f400e4a4a796765c61d1f5f4b3a4a7b705464f
Takashi SASAKI <takashi316@gmail.com>
2016-01-27 23:05:07 +0900

    StripeFragmentActivityTest passed

---
+++ app/src/androidTest/java/com/gmail/takashi316/tminchart/stripe/StripeFragmentActivityTest.java
@@ -0,0 +1,26 @@
+package com.gmail.takashi316.tminchart.stripe;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class StripeFragmentActivityTest {
+    @Rule
+    public ActivityTestRule<StripeFragmentActivity> activityTestRule = new ActivityTestRule<>(StripeFragmentActivity.class);
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void createActivity() {
+    }
+}
