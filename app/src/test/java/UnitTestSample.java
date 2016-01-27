e7fa5dd2d2c17aa4adc160e581615ad8ace223f6
Takashi SASAKI <takashi316@gmail.com>
2016-01-27 17:03:42 +0900

    A sample of unit test with Mockito

---
+++ app/src/test/java/UnitTestSample.java
@@ -0,0 +1,47 @@
+import android.content.Context;
+
+import com.gmail.takashi316.tminchart.R;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UnitTestSample {
+
+    private static final String FAKE_STRING = "HELLO WORLD";
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+        // Given a mocked Context injected into the object under test...
+        when(mMockContext.getString(R.string.hello_world))
+                .thenReturn(FAKE_STRING);
+        ClassUnderTest myObjectUnderTest = new ClassUnderTest(mMockContext);
+
+        // ...when the string is returned from the object under test...
+        String result = myObjectUnderTest.getHelloWorldString();
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_STRING));
+    }
+}
+
+class ClassUnderTest {
+    Context context;
+
+    ClassUnderTest(Context context) {
+        this.context = context;
+    }
+
+    String getHelloWorldString() {
+        return context.getString(R.string.hello_world);
+    }
+}
